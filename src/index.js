0e63f0cbb5215174b85dd609565f6256cf9b95ee
mansurjon.anorboev <mansurjon.anorboev@ICT-OR-SP>
2019-07-03 09:37:01 +0500

    some optimizations

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
   const brackets = bracketsConfig.map(x => x[0] + x[1]);
-
-  while(str.length > 1){
-    var oldStr = str;
+  var oldStr;
+  
+  while(oldStr != str){
+    oldStr = str;
     brackets.forEach(bracket => {
       str = str.replace(bracket, '');
     });
-    if(oldStr == str)
-      break;
   }
-  
+
   return str.length == 0;
 }
