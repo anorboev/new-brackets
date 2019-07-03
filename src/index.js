d67b9b0e8c5af7710b05c7e35df401da456b51fc
mansurjon.anorboev <mansurjon.anorboev@ICT-OR-SP>
2019-07-03 09:32:51 +0500

    finished task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets = bracketsConfig.map(x => x[0] + x[1]);
+
+  while(str.length > 1){
+    var oldStr = str;
+    brackets.forEach(bracket => {
+      str = str.replace(bracket, '');
+    });
+    if(oldStr == str)
+      break;
+  }
+  
+  return str.length == 0;
 }
