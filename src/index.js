a6bcadbccd56ebc10f16f3c405477d033d8eeb0a
Mansur <anorboyev@gmail.com>
2019-10-16 19:51:42 +0500

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = function check(str, bracketsConfig) {
   const brackets = bracketsConfig.map(x => x[0] + x[1]);
-  var oldStr;
+  let oldStr;
   
   while(oldStr != str){
     oldStr = str;
